3226404b119d48d4e16054b5c1684a7087cd4bf0
chandana M <chandu11288@gmail.com>
2018-04-18 14:19:18 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,6 @@
+class Artist
+  attr_accessor :name
+  def intialize(name = Artist)
+    @name = name
+  end
+end
---
+++ lib/author.rb
@@ -0,0 +1,6 @@
+class Author
+  attr_accessor :name
+  def intialize(name = Author)
+    @name = name
+  end
+end
